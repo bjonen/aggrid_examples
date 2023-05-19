496efb4019605a05261f7118ce56eea2aa3ebec1
Benjamin Jonen <bjonen@gmail.com>
2023-05-19 02:49:51 +0200

    Add various examples

---
+++ apps/app1/index.jsx
@@ -0,0 +1,84 @@
+"use strict";
+
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+const getServerSideDatasource = (server) => {
+  return {
+    getRows: (params) => {
+      console.log("[Datasource] - rows requested by grid: ", params.request);
+      // get data for request from our fake server
+      var response = server.getData(params.request);
+      // simulating real server call with a 500ms delay
+      setTimeout(function () {
+        if (response.success) {
+          // supply rows for requested block to grid
+          params.success({
+            rowData: response.rows,
+            rowCount: response.lastRow,
+          });
+        } else {
+          params.fail();
+        }
+      }, 500);
+    },
+  };
+};
+
+const GridExample = () => {
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "athlete", minWidth: 220 },
+    { field: "country", minWidth: 200 },
+    { field: "year" },
+    { field: "sport", minWidth: 200 },
+    { field: "gold" },
+    { field: "silver" },
+    { field: "bronze" },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      // all columns have sorting enabled
+      sortable: true,
+      flex: 1,
+      minWidth: 100,
+      resizable: true,
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .then((resp) => resp.json())
+      .then((data) => {
+        // setup the fake server with entire dataset
+        var fakeServer = new FakeServer(data);
+        // create datasource with a reference to the fake server
+        var datasource = getServerSideDatasource(fakeServer);
+        // register the datasource with the grid
+        params.api.setServerSideDatasource(datasource);
+      });
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={gridStyle} className="ag-theme-alpine-dark">
+        <AgGridReact
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          rowModelType={"serverSide"}
+          animateRows={true}
+          onGridReady={onGridReady}
+        ></AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<GridExample />);
